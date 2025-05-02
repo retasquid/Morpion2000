25e6523c1e5e4fc06a830c0cbd04bbeccb3b7928
théo <91142943+AsunaSungJinWoo@users.noreply.github.com>
2025-05-02 19:04:20 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const imageUrl = localStorage.getItem('profileImage');
     const userName = localStorage.getItem('userName');
-
+	const onlineButton = document.querySelector('.boutons .btn:nth-child(2)'); // Sélectionne le bouton ONLINE
+    const overlay = document.getElementById('overlay');
+    const notificationPopup = document.getElementById('notificationPopup');
+    const closeButton = document.getElementById('closeButton');
+	
     if (imageUrl) {
         const profileImg = document.getElementById('profileImg');
         if (profileImg) {
@@ -15,4 +19,22 @@ document.addEventListener('DOMContentLoaded', function () {
             pseudoElem.textContent = userName;
         }
     }
+
+    
+    // Fonction pour afficher la popup
+    function showPopup() {
+        overlay.style.display = 'block';
+        notificationPopup.style.display = 'block';
+    }
+    
+    // Fonction pour fermer la popup
+    function closePopup() {
+        overlay.style.display = 'none';
+        notificationPopup.style.display = 'none';
+    }
+    
+    // Ajouter des écouteurs d'événements
+    onlineButton.addEventListener('click', showPopup);
+    closeButton.addEventListener('click', closePopup);
+    overlay.addEventListener('click', closePopup);
 });
